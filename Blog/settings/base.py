cd97503de7e827b202dff7f1813dd9423ec05efc
ahaoao <18985242014@163.com>
2020-01-16 15:35:49 +0800

    '注册APP和使用Mysql'

--- Blog/settings/base.py
+++ Blog/settings/base.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appBlog',
+    'appConfig',
+    'appComment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
